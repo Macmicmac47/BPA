a46195867a7e27ccfcf943330a6f3c528493aa83
Abou Diaby <ab.diaby@gmail.com>
2019-10-26 12:26:45 -0500

    initial commit of the biztalk solution

---
+++ GMD.Biztalk/GMB.Biztalk.PipelineComponents/PipelineComponentBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.BizTalk.Component.Interop;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GMB.Biztalk.PipelineComponents
+{
+    public abstract class PipelineComponentBase : IPersistPropertyBag
+    {
+        private static readonly ConcurrentDictionary<Type, XmlSerializer> serialisers = new ConcurrentDictionary<Type, XmlSerializer>();
+        public abstract void GetClassID(out Guid classID);
+
+
+        public abstract void InitNew();
+       
+
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+          
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
